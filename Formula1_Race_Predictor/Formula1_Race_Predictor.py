370b91b8a0f53440f8719c93fcea70e37ab169bd
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2019-08-25 15:07:17 -0400

    Defined deep neural net architecture

--- Formula1_Race_Predictor/Formula1_Race_Predictor.py
+++ Formula1_Race_Predictor/Formula1_Race_Predictor.py
@@ -1,18 +1,20 @@
 import pandas as pd
-import dataset_formation, data_preprocessing
+import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+import dataset_formation, data_preprocessing, dnn_arch
 
 # Form dataset and save it (commented out after saving)
-# pre_race_df = dataset_formation.create_datasets()
-
-# Using only pre-race data to make predictions for now
-pre_race_df = pd.read_csv("../Datasets/pre_race_df.csv", header=0, index_col=0)
+pre_race_df = dataset_formation.create_datasets()
 
 # Remove NaN, Normalize, generate train, val and test sets
 values = data_preprocessing.preprocess_df(pre_race_df)
 train_x, train_y, val_x, val_y, test_x, test_y = data_preprocessing.generate_dnn_data(values)
 
-# Form deep neural net architecture
-# Compile/Train neural net
+# Form/compile deep neural net architecture
+dnn_model = dnn_arch.form_dnn()
+
+# Train/Validate model
+
 # Tensorboard validation
 # Test with test data
 # Save model
