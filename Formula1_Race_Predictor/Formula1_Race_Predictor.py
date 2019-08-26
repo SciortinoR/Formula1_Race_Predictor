f2ffdf7d7d08a10330eadfb52cfbcb65a6edb02c
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2019-08-26 10:20:36 -0400

    Added prediction and scoring of model.

--- Formula1_Race_Predictor/Formula1_Race_Predictor.py
+++ Formula1_Race_Predictor/Formula1_Race_Predictor.py
@@ -1,7 +1,9 @@
 import datetime
+import numpy as np
 import tensorflow as tf
 import dataset_formation, data_preprocessing, dnn_arch
 
+from numpy import concatenate
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 
 # Directories for models & logs
@@ -16,7 +18,7 @@ values = data_preprocessing.preprocess_df(pre_race_df)
 train_x, train_y, val_x, val_y, test_x, test_y, class_weights = data_preprocessing.generate_dnn_data(values)
 
 # Form/compile deep neural net architecture
-dnn_model = dnn_arch.form_dnn(train_x[0].shape)
+dnn_model = dnn_arch.form_dnn(train_x[0].shape, len(np.unique(train_y)))
 
 # Initialize Tensorboard and ModelCheckpoint callbacks 
 tensorboard = TensorBoard(log_dir=logs_dir)
@@ -26,19 +28,24 @@ checkpoint = ModelCheckpoint(models_dir, monitor='val_accuracy', verbose=1, save
 dnn_model.fit(
     train_x, train_y,
     batch_size=64,
-    epochs=15,
+    epochs=100,
     class_weight=class_weights,
     validation_data=(val_x, val_y),
     callbacks=[tensorboard, checkpoint])
 
 # Load model once saved (can use currently trained dnn_model as well)
-loaded_model = tf.keras.models.load_model('../Models/DNN_Final-14-0.49.hdf5')
+# loaded_model = tf.keras.models.load_model('../Models/DNN_Final-15-0.50.hdf5')
 
-# Predict Output/Probability Distributions
-predictions = loaded_model.predict(test_x)
-print('First prediction: ', predictions[0])
+# Predict Output/Probability Distributions 
+# (Reverse normalization on sample to see which sample predicting)
+predictions = dnn_model.predict(test_x)
+ty_reshaped = test_y.reshape(test_y.shape[0], 1)
+tx_conc = concatenate((test_x, ty_reshaped), axis=1)
+tx_conc = data_preprocessing.scaler.inverse_transform(tx_conc)
+print('Sample Data: ', tx_conc[1838])
+print('Sample Prediction: ', predictions[1838])
 
 # Test/Score Model
-score = loaded_model.evaluate(test_x, test_y, verbose=0)
+score = dnn_model.evaluate(test_x, test_y, verbose=0)
 print('Test Loss: ', score[0])
 print('Test Accuracy: ', score[1])
